81a5f7d0b0f320be2eba095dc350e0c9938bd02c
Jasper Stam <jasper@strnk.nl>
2018-09-02 16:00:34 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import * as talib from 'talib';
+
+talib.execute({
+    name: 'EMA',
+    startIdx: 0,
+    endIdx: 5,
+    inReal: [1, 2, 3, 4, 5],
+    optInTimePeriod: 5,
+}, function(err, result) {
+    console.log('ema result', err, result);
+});
